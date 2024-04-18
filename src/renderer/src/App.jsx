c66b069fa5c1454d9b562017f522bb767c3d0292
pmz10 <welcome_to_pmz@hotmail.com>
2024-04-18 15:23:32 -0600

    Modelo Desktop APP

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonGenerico from "./components/ButtonGenerico"
+
+
+const App = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <ButtonGenerico />
+      <ButtonGenerico />
+      <ButtonGenerico />
+      <ButtonGenerico />
+    </>
+  )
+}
+
+export default App
---
+++ src/renderer/src/components/ButtonGenerico.jsx
@@ -0,0 +1,11 @@
+import "../Styles/styleButtonGenerico.css"
+
+const ButtonGenerico = () => {
+  return (
+    <>
+      <input type="button" class="custom-button" value="Inicia Sesión"></input>
+    </>
+  )
+}
+
+export default ButtonGenerico
